53571c685cf07ff6b49765b12b4690780666d87a
thibaultmarq <114607476+thibaultmarq@users.noreply.github.com>
2024-12-11 10:45:22 +0100

    modification debugmanager

--- Assets/Scripts/Magic/DebugManager.cs
+++ Assets/Scripts/Magic/DebugManager.cs
@@ -1,47 +1,70 @@
+using Meta.WitAi;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Material = Magic.Materials.Material;
 
 public class DebugManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] prefabs;
+
+    [SerializeField] private GameObject[] prefabs2;
+
+
+    private void Start()
+    {
+        transform.position = new Vector3(0, 5, 0);
+
+    }
 
     private void Update()
     {
-        transform.position = new Vector3(0, 5, 0);
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            Instantiate(prefabs[0],transform);
+            Instantiate(prefabs2[0],transform.position,Quaternion.identity);
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
 
-            Instantiate(prefabs[1], transform);
+            Instantiate(prefabs2[1], transform.position, Quaternion.identity);
         }        else if (Input.GetKeyDown(KeyCode.E))
         {
 
-            Instantiate(prefabs[2], transform);
+            Instantiate(prefabs2[2], transform.position, Quaternion.identity);
         }        else if (Input.GetKeyDown(KeyCode.R))
         {
 
-            Instantiate(prefabs[3], transform);
+            Instantiate(prefabs2[3], transform.position, Quaternion.identity);
         }        else if (Input.GetKeyDown(KeyCode.T))
         {
 
-            Instantiate(prefabs[4], transform);
+            Instantiate(prefabs2[4], transform.position, Quaternion.identity);
         }        else if (Input.GetKeyDown(KeyCode.Y))
         {
 
-            Instantiate(prefabs[5], transform);
+            Instantiate(prefabs2[5], transform.position, Quaternion.identity);
         }else if (Input.GetKeyDown(KeyCode.U))
         {
-            foreach (Transform child in this.transform)
+            foreach (GameObject child in GameObject.FindGameObjectsWithTag("Material"))
             {
 
-                Destroy(child.gameObject);
+                Destroy(child);
             }
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) { transform.position += new Vector3(-0.5f, 0 , 0);
+
+        }
+         else if (Input.GetKeyDown(KeyCode.RightArrow )) { transform.position += new Vector3(0.5f, 0 , 0);
+
+
+        }
+         else if (Input.GetKeyDown(KeyCode.UpArrow)) { transform.position += new Vector3(0, 0 , 0.5f);
+
+
+        }
+         else if (Input.GetKeyDown(KeyCode.DownArrow)) { transform.position += new Vector3(0, 0 ,-0.5f);
+
         }
     }
 }
