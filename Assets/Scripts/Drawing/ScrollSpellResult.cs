d532d2979b383a2b3bef1382054393023149dcd7
Victor Ly <lyvictor1@gmail.com>
2024-12-12 13:24:46 +0100

    Modified Sandbox stage and different materials

--- Assets/Scripts/Drawing/ScrollSpellResult.cs
+++ Assets/Scripts/Drawing/ScrollSpellResult.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
 
+#nullable enable
+
 public class ScrollSpellResult : MonoBehaviour
 {
     public static event System.Action<string, string> OnSpellReady;
-    private string currentSpellElement;
-    [SerializeField] private Scroll scroll;
+    [SerializeField] private string currentSpellElement = "fire";
+    [SerializeField] private Scroll? scroll;
+    private Vector3 spawnCoords;
+
+    public void Start()
+    {
+        spawnCoords = this.gameObject.transform.position;
+    }
 
     public void OnSelect()
     {
         OnSpellReady?.Invoke(currentSpellElement, "RightHand");
-        scroll.EraseScroll();
+        scroll?.EraseScroll();
         this.gameObject.GetComponent<Renderer>().enabled = false;
-        //this.gameObject.SetActive(false);
     }
 
     public void OnUnselect()
     {
         this.gameObject.SetActive(false);
+    }
+
+    public void Reset()
+    {
+        this.gameObject.transform.position = spawnCoords;
+        this.gameObject.GetComponent<Renderer>().enabled = true;
+        this.gameObject.SetActive(true);
     }
 
     public void SetCurrentSpellElement(string element)
--- Assets/Scripts/ShootingManager.cs
+++ Assets/Scripts/ShootingManager.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootingManager : MonoBehaviour
 {
     [SerializeField] private GameObject spellPrefab;
+    [SerializeField] private List<Material> spellMaterials;
+
     [SerializeField] private Transform leftHandTransform;
     [SerializeField] private Transform rightHandTransform;
 
@@ -67,25 +70,52 @@ public class ShootingManager : MonoBehaviour
         {
             case "fire":
                 spell.AddComponent<FireSpell>();
-                spell.GetComponent<Renderer>().material.color = Color.red;
+                spell.GetComponent<Renderer>().material = spellMaterials[0];
                 break;
             case "water":
                 spell.AddComponent<WaterSpell>();
-                spell.GetComponent<Renderer>().material.color = Color.blue;
-                break;
-            case "wind":
-                spell.AddComponent<WindSpell>();
-                spell.GetComponent<Renderer>().material.color = Color.green;
+                spell.GetComponent<Renderer>().material = spellMaterials[1];
                 break;
             case "earth":
                 spell.AddComponent<EarthSpell>();
-                spell.GetComponent<Renderer>().material.color = Color.gray;
+                spell.GetComponent<Renderer>().material = spellMaterials[2];
                 break;
+            case "wind":
+                spell.AddComponent<WindSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[3];
+                break;
+            case "ice":
+                spell.AddComponent<IceSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[4];
+                break;
+            case "plant":
+                spell.AddComponent<PlantSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[5];
+                break;
+            case "thunder":
+                spell.AddComponent<ThunderSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[6];
+                break;
+            case "volcano":
+                spell.AddComponent<VolcanoSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[7];
+                break;
+            case "steam":
+                spell.AddComponent<SteamSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[8];
+                break;
+            case "sand":
+                spell.AddComponent<SandSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[9];
+                break;
+            
             default:
                 Debug.LogWarning($"Aucun script trouvé pour l'élément {element}. Sort par défaut utilisé.");
+                spell.AddComponent<FireSpell>();
+                spell.GetComponent<Renderer>().material = spellMaterials[0];
                 break;
         }
 
-        spell.GetComponent<Rigidbody>().AddForce((-handTransform.up*0.4f + handTransform.right*0.6f) * 900);
+        spell.GetComponent<Rigidbody>().AddForce((-handTransform.up*0.4f + handTransform.right*0.6f) * 800);
     }
 }
