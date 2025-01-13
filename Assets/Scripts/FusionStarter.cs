8be60c46c5c468694e446d26d800c2c88d7fd9e9
Xiran Kali <lucas.silver999@gmail.com>
2025-01-13 17:05:57 +0100

    Added Fusion of spells

---
+++ Assets/Scripts/FusionStarter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FusionStarter : MonoBehaviour
+{
+
+    [SerializeField]
+    private ShootingManager receiver;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Hand")
+        {
+            receiver.FuseSpell();
+        }
+
+    }
+        
+    
+}
