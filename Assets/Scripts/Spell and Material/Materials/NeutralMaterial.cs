0675da5c36787250e83c982fd08362f9378e96bc
thibaultmarq <114607476+thibaultmarq@users.noreply.github.com>
2025-01-26 22:31:22 +0100

    correction softlock lvl 2

---
+++ Assets/Scripts/Spell and Material/Materials/NeutralMaterial.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Material = Magic.Materials.Material;
+public class NeutralMaterial : Material
+{
+    // Start is called before the first frame update
+
+}
