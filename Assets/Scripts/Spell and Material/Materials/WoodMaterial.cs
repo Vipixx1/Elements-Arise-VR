ad8112e0ca75f7c0aa849c444b6bc266a3ca4bf9
Victor Ly <lyvictor1@gmail.com>
2025-01-27 17:03:15 +0100

    Lights of Level 1 and wood script modified

--- Assets/Scripts/Spell and Material/Materials/WoodMaterial.cs
+++ Assets/Scripts/Spell and Material/Materials/WoodMaterial.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Material = Magic.Materials.Material;
 
+public enum Axis
+{
+    x,
+    y,
+    z
+}
 public class WoodMaterial : Material
 {
     private float scaling;
@@ -10,17 +16,28 @@ public class WoodMaterial : Material
     private bool isBurning = false;
     private bool isGrowing = false;
 
+    [SerializeField] private Axis axisToBurn;
+
     private void Start()
     {
-        scaling = transform.localScale.x;
+        switch (axisToBurn)
+        {
+            case Axis.x:
+                scaling = transform.localScale.x;
+                break;
+            case Axis.y:
+                scaling = transform.localScale.y;
+                break;
+            case Axis.z:
+                scaling = transform.localScale.z;
+                break;
+        }
         maxScaling *= scaling;
     }
 
 
     private void Update()
     {
-
-
         if (isGrowing && scaling < maxScaling)
         {
             scaling += Time.deltaTime;
@@ -31,7 +48,6 @@ public class WoodMaterial : Material
         }
         if (isBurning)
         {
-
             scaling -= Time.deltaTime;
             if (scaling <= 0)
             {
@@ -40,26 +56,31 @@ public class WoodMaterial : Material
 
         }
         Vector3 newScale = gameObject.transform.lossyScale;
-        newScale.x = scaling;
+        switch (axisToBurn)
+        {
+            case Axis.x:
+                newScale.x = scaling;
+                break;
+            case Axis.y:
+                newScale.y = scaling;
+                break;
+            case Axis.z:
+                newScale.z = scaling;
+                break;
+        }
         gameObject.transform.localScale = newScale;
-
-
     }
-
 
     public override void Burning(ObjectData data)
     {
         base.Burning(data);
         isBurning = true;
-        Debug.Log(isBurning);
     }
 
     public override void OnPlant(ObjectData data, float[] args = null)
     {
         if (!isBurning)
-        {
             isGrowing = true;
-        }
     }
 
     public override void OnIce(ObjectData data, float[] args = null)
@@ -67,19 +88,14 @@ public class WoodMaterial : Material
         base.OnIce(data);
         isGrowing = false;
         if (data.Humidity > 0 || data.Temperature < 1)
-        {
             isBurning = false;
-        }
-
     }
 
     public override void OnWater(ObjectData data, float[] args = null)
     {
         base.OnWater(data);
         if (data.Humidity > 0 || data.Temperature < 1)
-        {
             isBurning = false;
-        }
     }
 
 }
