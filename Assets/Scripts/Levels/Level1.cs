9a2f1ba7893447b466cbc8dd04ccec10e3d90381
Adam <62399795+Adamme-le-codeur-fou@users.noreply.github.com>
2025-01-28 22:10:27 +0100

    fix rock falling and level1 end not reachable

--- Assets/Scripts/Levels/Level1.cs
+++ Assets/Scripts/Levels/Level1.cs
@@ -2,21 +2,20 @@ using Oculus.Interaction.Locomotion;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Level1 : MonoBehaviour
 {
     [SerializeField] GameObject floorOfRoom2;
-    [SerializeField] GameObject[] planks;
+    [SerializeField] List<GameObject> planks;
 
     private void Update()
     {
-        if (!planks.Any())
+        if (planks.Exists((elt) => !elt.IsUnityNull())) return;
+        foreach (Transform carpet in floorOfRoom2.transform)
         {
-            foreach (Transform carpet in floorOfRoom2.transform)
-            {
-                SetInteractable(carpet.gameObject);
-            }
+            SetInteractable(carpet.gameObject);
         }
     }
 
